d65168f814352b5e966e4ab15fa166f84f4c589d
sucodlaranja <joaoandre210@gmail.com>
2022-04-05 00:14:28 +0100

    barra filtros do portfolio funcionalidade done

--- src/pages/portfolio/portfolio.js
+++ src/pages/portfolio/portfolio.js
@@ -1,8 +1,25 @@
 import TopBar from "../../components/topBar/topBar";
 import './portfolio.css'
 import projects from '../../data/projects.json'
+import { useState } from "react";
 function Portfolio() {
+  const [state, setState] = useState(projects);
 
+  function changeGrid(language) {
+    var temp = []
+    if (language === "All") {
+      temp = projects
+    }
+    else {
+      for (var i = 0; i < projects.length; i++) {
+        console.log(projects[i])
+        if (projects[i].language.includes(language)) {
+          temp.push(projects[i])
+        }
+      }
+    }
+    setState(temp)
+  }
 
   return (
     <div>
@@ -10,18 +27,18 @@ function Portfolio() {
 
       <div className="portfolio-container">
         <div className="language-container">
-          <a className="language language-first" href="">C</a>
-          <a className="language" href="">Prolog</a>
-          <a className="language" href="">Java</a>
-          <a className="language" href="">Javascript</a>
-          <a className="language" href="">Flutter</a>
+          <button onClick={() => changeGrid("All")} className="language language-first">All</button>
+          <button onClick={() => changeGrid("C")} className="language" >C</button>
+          <button onClick={() => changeGrid("Prolog")} className="language" >Prolog</button>
+          <button onClick={() => changeGrid("Java")} className="language" >Java</button>
+          <button onClick={() => changeGrid("Javascript")} className="language" >Javascript</button>
+          <button onClick={() => changeGrid("Flutter")} className="language" >Flutter</button>
 
         </div>
         <div className="grid-container">
-          {projects.map((project) =>
-            <div className="square-container" style={{backgroundImage: `url(${project["image-src"]})`}}>
-              {project.name}
-              
+          {state.map((project) =>
+            <div className="square-container" style={{ backgroundImage: `url(${project["image-src"]})` }}>
+              <div className="square-text">{project.name}</div>
             </div>
           )}
 
