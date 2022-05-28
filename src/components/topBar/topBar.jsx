ed2dbbc5d5d3b51a5136f42b0e6fbee7c3fba8f2
sucodlaranja <joaoandre210@gmail.com>
2022-05-28 21:01:00 +0100

    melhor organizacao do menu e melhorei as animacoes

--- src/components/topBar/topBar.jsx
+++ src/components/topBar/topBar.jsx
@@ -1,43 +1,67 @@
 import "./topBar.css";
 import { useState } from "react";
-import { useTransition, animated } from 'react-spring'
+import { useTransition, animated, config } from 'react-spring'
 import { Link } from "react-router-dom"
 
 function TopBar() {
   const [menu, setMenu] = useState(false);
-  const transition = useTransition(menu, {
-    from: { x: 0, y: 0, opacity: 0 },
+  const [middle, setmiddle] = useState(false);
+  const transition = useTransition(middle, {
+    from: { x: -150, y: 0, opacity: 0 },
     enter: { x: 0, y: 0, opacity: 1 },
-    leave: { x: 0, y: 0, opacity: 0 },
+    leave: { x: -150, y: 0, opacity: 0 },
+
   })
 
   function changeMenu() {
-    var visibility = !menu;
-    setMenu(visibility);
+    var visibility = !middle;
+    setmiddle(visibility);
+    if (!menu) {
+      setMenu(!menu)
+    }
+    else {
+      setTimeout(() => setMenu(!menu), 200)
+    }
   }
 
   function showMenu() {
     return (
       <div>
-        <button className="menu-Title" onClick={() => changeMenu()}>
-          Menu
-        </button>
-        <div className="menu-container">
-          {transition((styles, item) => item && <animated.div style={styles}><button className="menu-Title" onClick={() => changeMenu()} >Back</button>
-            <div className="menu-content">
-              <p className="Home-Button"><Link to='/'>Home</Link></p>
-              <p>CV</p>
-              <p><Link to="/about">About me</Link></p>
-              <p><Link to="/portfolio">Portfolio</Link></p>
-            </div></animated.div>)}
+        {!menu &&
+          <div className="topBar-Container">
+            <button className="menu-Title" onClick={() => changeMenu()}>
+              Menu
+            </button>
+          </div>
+        }
+        {menu && <div>
+
+          <div className="topBarOn-Container">
+            <button className="menu-Title" onClick={() => changeMenu()} >Back</button>
+          </div>
+
+          <div className="menu-container">
+            {transition((styles, item) => item && <animated.div style={styles}>
+
+              <div className="menu-content">
+                <p className="Home-Button"><Link to='/'>Home</Link></p>
+                <p>CV</p>
+                <p><Link to="/about">About me</Link></p>
+                <p><Link to="/portfolio">Portfolio</Link></p>
+              </div>
+            </animated.div>)}
+          </div>
+
         </div>
-      </div>);
+        }
+      </div>
+    );
   }
 
   return (
-    <div className="topBar-Container">
-      <div >{showMenu()}</div>
-    </div>
+
+    <div >{showMenu()}</div>
+
   );
 }
 
